1ac88ef43c2a0c63fe1bea51b006273780ce0782
Antonio Unabia <ajunabia228@gmail.com>
2026-06-14 18:01:34 -0500

    added more paragraph prompts

--- js/paragraphs.js
+++ js/paragraphs.js
@@ -30,4 +30,5 @@ const paragraphs = [
   "The cost of a thing is the amount of what I will call life which is required to be exchanged for it, immediately or in the long run.",
   "In the long run, the sharpest weapon of all is a kind and gentle spirit.",
   "The things we truly love stay with us always, locked in our hearts as long as life remains.",
+  "In the sky there are always answers and explanations for everything: every pain, every suffering, joy and confusion."
 ];
